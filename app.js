1d3ff60a622c106179d8c4bd9d7f406c32323659
Sam <re711kimo@gmail.com>
2020-06-29 18:16:34 +0800

    Add restaurant index page

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ app.set('view engine', 'hbs')
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
+
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app)

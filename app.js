f71728069604b714c9c1530f938717f3a4da6c20
Sam <re711kimo@gmail.com>
2020-06-30 14:26:38 +0800

    Add userController and signup page and install bcryptjs body-parser

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)

3d31e9dccab182d947f6eec826d65be32b2d4d96
Sam <re711kimo@gmail.com>
2020-06-30 14:39:55 +0800

    Install express-session connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
@@ -8,6 +10,8 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)

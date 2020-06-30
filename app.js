c338ab476253870562706c3017abf9d836977621
Sam <re711kimo@gmail.com>
2020-06-30 13:08:22 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))

f2f5a652408aa3d7dde8045b1651d420d7601395
Sam <re711kimo@gmail.com>
2020-07-06 16:05:51 +0800

    Add handlebar helper ifCond

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))

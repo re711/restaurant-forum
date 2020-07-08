51b7dad3ca44747509ef896a31c626b0e6e55eed
Sam <re711kimo@gmail.com>
2020-07-08 23:42:33 +0800

    Update seed informatiom

--- seeders/20200701064349-my-seed-file.js
+++ seeders/20200701064349-my-seed-file.js
@@ -11,21 +11,24 @@ module.exports = {
       isAdmin: true,
       name: 'root',
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
+      image: `https://loremflickr.com/g/320/240/paris/?random=${Math.random() * 5}`
     }, {
       email: 'user1@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user1',
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
+      image: `https://loremflickr.com/g/320/240/paris/?random=${Math.random() * 5}`
     }, {
       email: 'user2@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user2',
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
+      image: `https://loremflickr.com/g/320/240/paris/?random=${Math.random() * 5}`
     }], {})
 
     // generate category seed data

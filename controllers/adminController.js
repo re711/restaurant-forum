d65abc352f4a274ffb1209b2ec4e5637dd66186e
Sam <re711kimo@gmail.com>
2020-07-13 23:27:25 +0800

    refactor adminController.getRestaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 const db = require('../models')
 const imgur = require('imgur-node-api')
+const adminService = require('../services/adminService')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const Restaurant = db.Restaurant
 const User = db.User
@@ -8,12 +9,8 @@ const Category = db.Category
 const adminController = {
   // 餐廳後台
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    }).then(restaurants => {
-      return res.render('admin/restaurants', { restaurants: restaurants })
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants', data)
     })
   },
 
---
+++ services/adminService.js
@@ -0,0 +1,21 @@
+const db = require('../models')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const Restaurant = db.Restaurant
+const User = db.User
+const Category = db.Category
+
+const adminService = {
+  // 餐廳後台
+  getRestaurants: (req, res, callback) => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurants => {
+      callback({ restaurants: restaurants })
+    })
+  }
+}
+
+module.exports = adminService

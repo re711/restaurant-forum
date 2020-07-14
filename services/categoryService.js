5bdb998ed2565426c05fb42507b947f5903f7b26
Sam <re711kimo@gmail.com>
2020-07-14 15:50:23 +0800

    refactor categoryController.getCategories

---
+++ services/categoryService.js
@@ -0,0 +1,25 @@
+const db = require('../models')
+const Category = db.Category
+
+const categoryController = {
+  getCategories: (req, res, callback) => {
+    return Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      if (req.params.id) {
+        Category.findByPk(req.params.id)
+          .then(category => {
+            return res.render('admin/categories', {
+              categories: categories,
+              category: category.toJSON()
+            })
+          })
+      } else {
+        callback({categories: categories})
+      }
+    })
+  },
+}
+
+module.exports = categoryController
